cdaa1a6a646601adbbe4c54c390f1c6abc7300d7
R0rt1z2 <me@r0rt1z2.com>
2026-01-03 17:54:24 -0500

    sprig: Add and use ARM64 MMIO helpers

--- payload/main.c
+++ payload/main.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 
 #include <debug.h>
+#include <mmio.h>
 
 struct bldr_command_handler {
     void *priv;
@@ -15,20 +16,6 @@ typedef struct {
     const char *name;
 } patch_t;
 
-static inline void flush_dcache_range(unsigned long start, unsigned long size) {
-    unsigned long end = start + size;
-    for (unsigned long addr = start; addr < end; addr += 64) {
-        __asm__ volatile("dc cvac, %0" :: "r"(addr) : "memory");
-    }
-    __asm__ volatile("dsb sy" ::: "memory");
-}
-
-static inline void invalidate_icache(void) {
-    __asm__ volatile("ic ialluis" ::: "memory");
-    __asm__ volatile("dsb sy" ::: "memory");
-    __asm__ volatile("isb" ::: "memory");
-}
-
 static const patch_t patches[] = {
     { .addr = 0x02043f08, .patch = {0x529c401c, 0x00000000}, .name = "Increase handshake timeout" },
     { .addr = 0x0206c49c, .patch = {0x2a1f03e0, 0xd65f03c0}, .name = "Do not verify DA1" },
@@ -39,17 +26,17 @@ static const patch_t patches[] = {
 
 static void apply_patches(void) {
     for (int i = 0; i < sizeof(patches) / sizeof(patches[0]); i++) {
-        volatile uint32_t *addr = (volatile uint32_t *)(uintptr_t)patches[i].addr;
+        uint32_t addr = patches[i].addr;
         
-        uint32_t before = addr[0];
+        uint32_t before = readl(addr);
         
-        addr[0] = patches[i].patch[0];
+        writel(patches[i].patch[0], addr);
         if (patches[i].patch[1] != 0) {
-            addr[1] = patches[i].patch[1];
+            writel(patches[i].patch[1], addr + 4);
         }
         
-        flush_dcache_range((uint64_t)addr, 64);
-
+        flush_dcache_range(addr, 64);
+        
         if (patches[i].patch[1] != 0) {
             printf("Applying patch '%s': 0x%x -> 0x%x 0x%x\n", 
                     patches[i].name, before, patches[i].patch[0], patches[i].patch[1]);
@@ -85,7 +72,7 @@ void main(void) {
         .attr = 0,
         .cb = (void *)0x02048818
     };
-
+    
     printf("\nAbout to handshake...\n\n");
 
     ((int (*)(struct bldr_command_handler *))(0x02048404))(&handler);
